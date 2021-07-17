fca3277d8fbdde35ec0bb848a04db1b244771cf0
mridpin <mridao99@gmail.com>
2021-07-17 15:57:26 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <calculator />
+</template>
+
+<script>
+import Calculator from './components/calculator.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Calculator
+  }
+}
+</script>
---
+++ src/components/calculator.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+
+  <section class="section calculator">
+    <h1>calculator Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'Calculator',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
