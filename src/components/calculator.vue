3e28cffa36911a590e69c8e0549db06eb1416cab
mridpin <mridao99@gmail.com>
2021-07-17 16:45:38 +0200

    working on formula

--- src/components/calculator.vue
+++ src/components/calculator.vue
@@ -1,9 +1,36 @@
 <template lang="html">
-
   <section class="section calculator">
-    <h1>calculator Component</h1>
+    <div class="container">
+      <div class="field">
+        <label class="label">Ingresos netos mensuales:</label>
+        <div class="control">
+          <input
+            class="input"
+            type="number"
+            placeholder="1800"
+            min="0"
+            v-on:focusout="updateIncome"
+            v-model="income"
+          />
+        </div>
+        <p>¿No conoces tus ingresos netos? Calcúlalos aquí</p>
+      </div>
+    </div>
+    <div class="section container">
+      <div class="content is-large">
+        <ul>
+          <li>
+            La cuota mensual máxima que te puedes permitir es:
+            <strong>{{ income ? income * 0.3 : 0 }} euros</strong>
+          </li>
+          <li>
+            El precio máximo de la vivienda que puedes permitirte es:
+            <strong> {{ price }} euros</strong>
+          </li>
+        </ul>
+      </div>
+    </div>
   </section>
-
 </template>
 
 <script lang="js">
@@ -11,16 +38,25 @@
   export default  {
     name: 'Calculator',
     props: [],
-    mounted () {
-
+    created () {
     },
     data () {
       return {
-
+        price: 0
       }
     },
     methods: {
-
+      updateIncome() {
+        this.price = this.income * 0.30 * 360;
+        this.calculateMortgatePayment(100000.0, 1.0, 360);
+      },
+      calculateMortgatePayment(amount, ipa, n) {
+        const i = ipa / 12;
+        const a = (1-((1+i) ** (-n))) / i;
+        console.log(a)
+        const payments = amount / a;
+        console.log(payments);
+      }
     },
     computed: {
 
