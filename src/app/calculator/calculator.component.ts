f33876c30b61c753c9dfee0a4b66255eb780b65d
mridpin <ridaomanuel92@gmail.com>
2021-07-24 18:59:57 +0200

    finish mvp logic

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { InterestType, Mortgage, MortgageAdapter } from '../models/mortgage';
+import { MortgageCalculatorService } from '../services/mortgage-calculator.service';
 
 @Component({
   selector: 'app-calculator',
@@ -7,13 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  income!: number;
-  ipa!: number;
-  years!: number;
+  income!: string;
+  ipa!: string;
+  years!: string;
   monthlyPayment: number;
   maxHousePrice: number;
 
-  constructor() { 
+  constructor(
+    private mortgageCalc: MortgageCalculatorService,
+    private adapter: MortgageAdapter
+  ) {
     this.monthlyPayment = 0.0;
     this.maxHousePrice = 0.0;
   }
@@ -22,9 +27,23 @@ export class CalculatorComponent implements OnInit {
   }
 
   calculateMortgage(): void {
-    console.log(this.income);
-    console.log(this.ipa);
-    console.log(this.years);
+    this.monthlyPayment = this.mortgageCalc.calculateMonthlyPayment(parseFloat(this.income), 0.3);
+    const mortgage: Mortgage = this.adapter.adapt({
+      interestRate: this.ipa,
+      years: this.years,
+      type: InterestType.fixed,
+      percent: 0.8,
+      monthlyPayment: this.monthlyPayment,
+    });
+    const calculatedMortgage = this.mortgageCalc.calculateMaxPrincipal(mortgage);
+    this.maxHousePrice = calculatedMortgage.maxHousePrice;
   }
 
+  clearFields(): void {
+    this.income = '';
+    this.ipa = '';
+    this.years = '';
+    this.monthlyPayment = 0;
+    this.maxHousePrice = 0;
+  }
 }
--- src/app/services/mortgage-calculator.service.ts
+++ src/app/services/mortgage-calculator.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Mortgage } from '../models/mortgage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MortgageCalculatorService {
+
+  calculateMonthlyPayment(income: number, percentIncome: number): number {
+    return income * percentIncome;
+  }
+
+  calculateMaxPrincipal(mortgage: Mortgage): Mortgage {
+    const ipa: number = mortgage.interestRate;
+    const n: number = mortgage.years * 12;
+    const q: number = mortgage.monthlyPayment;
+
+    // amortizacion frances
+    const i = ipa / 12 / 100;
+    const denom = ((1 + i) ** -n);
+    const a = (1 - denom) / i;
+    const p = q * a;
+
+    mortgage.principal = p;
+    mortgage.maxHousePrice = mortgage.principal / 0.8;
+    return mortgage;
+  }
 
   constructor() { }
 }
